c533db6efc7de9c5064e6b0ed69d58c683bcdc3b
zhengjg <zhengjinguangzjg@163.com>
2024-04-02 16:54:07 +0800

    all page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,21 @@
 <template>
-  <div>
+  <div class="page-szjqh">
+    <big-wheel></big-wheel>
+    <slot-machine>
+      <span>老虎机抽奖</span>
+    </slot-machine>
+    <nine-square class="ninesquare-pmxig">
+      <span>卡牌抽奖</span>
+    </nine-square>
+    <score-task-list>
+      <span>任务列表</span>
+    </score-task-list>
     <header>
       <span>页面导航头</span>
     </header>
-    <big-wheel></big-wheel>
-    <big-wheel></big-wheel>
-    <big-wheel></big-wheel>
-    <big-wheel>
-      <span>按钮文本222</span>
-    </big-wheel>
-    <marquee>
-      <span>九宫格抽奖</span>
-    </marquee>
+    <launch>
+      <span>底部引导</span>
+    </launch>
   </div>
 </template>
 
@@ -20,7 +24,7 @@ import * as vue from 'vue'
 import { defineProps, defineEmits } from 'vue'
 import { I18nInjectionKey } from 'vue-i18n'
 import { span } from 'undefined'
-import { Header, BigWheel, Marquee } from 'ms-ui-lib'
+import { Header, BigWheel, NineSquare, SlotMachine, Launch, ScoreTaskList } from 'ms-ui-lib'
 
 const props = defineProps({})
 const emit = defineEmits([])
@@ -36,4 +40,18 @@ wrap({
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.ninesquare-pmxig {
+  margin-bottom: 60px;
+}
+.page-szjqh {
+  padding-bottom: 40px;
+  margin-top: 0px;
+  background-image: url(img/bgcModal.png), linear-gradient(#ffffff, #ffffff);
+  background-position: 0px 0px, 0px 0px;
+  background-size: auto, auto;
+  background-repeat: repeat, repeat;
+  background-attachment: undefined, undefined;
+  padding-top: 90px;
+}
+</style>
