9386f79038bad795188d83a0080a5edfd3fa7ec0
zhengjg <zhengjinguangzjg@163.com>
2024-07-03 19:29:20 +0800

    test6

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -6,9 +6,7 @@
       src="http://front-visual-bt1.oss-cn-shanghai.aliyuncs.com/icon/e17e9002-e913-4a55-815a-717624003a0e23.png"
     ></big-wheel>
     <score-task-list></score-task-list>
-    <etf-hot></etf-hot>
     <span class="text-gugwk" @click="redirectMyAward">我的礼包</span>
-    <etf-hot></etf-hot>
     <div class="div-fkgoy">
       <span class="ex-container">客服电话：95376</span>
     </div>
